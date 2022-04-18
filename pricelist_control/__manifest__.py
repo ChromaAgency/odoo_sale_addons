47257c2600cf612704adce1f8405dbe7165b7b80
Federico Gobea <65693362+fedegobea@users.noreply.github.com>
2022-04-18 10:17:14 -0300

    ➕ Add button hde and validation for when manager need approval

--- pricelist_control/__manifest__.py
+++ pricelist_control/__manifest__.py
@@ -28,6 +28,7 @@
 
     # always loaded
     'data': [
+        'views/sale.order.xml'
     ],
     # only loaded in demonstration mode
     'demo': [
--- pricelist_control/models/sale_order.py
+++ pricelist_control/models/sale_order.py
@@ -1,13 +1,44 @@
 from odoo.models import Model
-from odoo.api import onchange
+from odoo.exceptions import UserError
+from odoo.fields import Boolean
+from odoo.api import onchange, depends
+import logging
+_logger = logging.getLogger(__name__)
+class SaleOrderLine(Model):
+    _inherit = 'sale.order.line'
+        
+    not_aligned_with_pricelist = Boolean(string="No coincide con lista de precios", compute="_compute_not_aligned_with_pricelist")
+    
+    @depends('price_unit','discount')
+    def _compute_not_aligned_with_pricelist(self):
+        for rec in self:
+            rec.not_aligned_with_pricelist = rec._product_pricelist_price_differs_from_price_unit()
+
+    def _product_pricelist_price_differs_from_price_unit(self):
+        pricelist_price_unit = self.product_id._get_tax_included_unit_price(
+            self.company_id,
+            self.order_id.currency_id,
+            self.order_id.date_order,
+            'sale',
+            fiscal_position=self.order_id.fiscal_position_id,
+            product_price_unit=self._get_display_price(self.product_id),
+            product_currency=self.order_id.currency_id
+        )
+        if self.price_unit == pricelist_price_unit and self.discount == 0:
+            return False
+        return True
 
 class SaleOrder(Model):
     _inherit = 'sale.order'
     
-    @onchange('order_line', 'order_line.price_unit', 'order_line.discount')
-    def _onchange_orderline_prices(self):
-        for rec in self.order_line:
-            if rec.price_unit == rec._origin.price_unit and rec.discount == rec._origin.discount:
-                continue
-            self.show_update_pricelist = True
-            break
+    needs_manager_approval = Boolean(string="Necesita aprobación de gerente", compute="_compute_needs_manager_approval")
+
+    @depends('show_update_pricelist', 'order_line.not_aligned_with_pricelist')
+    def _compute_needs_manager_approval(self):
+        for rec in self:
+            rec.needs_manager_approval = any(rec.order_line.mapped('not_aligned_with_pricelist')) or rec.show_update_pricelist
+            
+    def action_confirm(self):
+        if self.needs_manager_approval:
+            raise UserError('Los precios no son los mismos que la lista de precios, por favor revisarlos o pedir aprobación de un gerente.')
+        return super().action_confirm()
