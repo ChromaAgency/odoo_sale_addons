eef7eec1a4b9ccd049a10b36cb20d4547b3e4e73
Federico Gobea <65693362+fedegobea@users.noreply.github.com>
2023-01-16 21:52:42 -0300

    ✨ Add new module for salesman permission

---
+++ sale_res_partner_permission/__manifest__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Permisos de vendedores extr<",
+
+    'summary': """
+    Module with advanced salesman permissions
+        """,
+
+    'description': """
+    """,
+
+    'author': "Making Argentina",
+    'installable': True,
+    'application':True,
+    'website': "http://www.making.com.ar",
+
+    'category': 'Sales',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['sale'],
+
+    # always loaded
+    'data': [
+        'security/ir.rule.xml',
+        'security/res.groups.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+    ],
+}
