a24436379ad7819449de45b8e30c85a1536d5c27
Federico Gobea <gitfgobea@gmail.com>
2023-07-19 22:09:59 -0500

    Added confirmed sale order count

--- crm_report_enhancement/models/crm_lead.py
+++ crm_report_enhancement/models/crm_lead.py
@@ -5,4 +5,8 @@ class CrmLead(Model):
     _inherit = "crm.lead"
 
     commercial_partner_id = Many2one("res.partner", string="Entidad del cliente", related="partner_id.commercial_partner_id", store=True)
-    customer_order_count = Integer(string="Cantidad de pedidos", related="commercial_partner_id.sale_order_count", store=True)
+    confirmed_sale_order_count = Integer(string="Cantidad de pedidos confirmados", compute="_compute_confirmed_sale_order_count", store=True)
+
+    def _compute_confirmed_sale_order_count(self):
+        for rec in self:
+            rec.confirmed_sale_order_count = len(rec.order_ids.filtered(lambda r: r.state in ["sale",'done']))
