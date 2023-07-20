6f0157e637748b5f1aae5c5a12bff70f740a9b25
Federico Gobea <gitfgobea@gmail.com>
2023-07-19 22:07:09 -0500

    💄 Add customer order_count

--- crm_report_enhancement/models/crm_lead.py
+++ crm_report_enhancement/models/crm_lead.py
@@ -1,13 +1,8 @@
 from odoo.models import Model
-from odoo.fields import Many2one
+from odoo.fields import Many2one, Integer
 
 class CrmLead(Model):
     _inherit = "crm.lead"
 
     commercial_partner_id = Many2one("res.partner", string="Entidad del cliente", related="partner_id.commercial_partner_id", store=True)
-    last_order_id = Many2one("sale.order", string="Última orden", compute="_compute_last_order_id", store=True)
-
-    def _compute_last_order_id(self):
-        for rec in self:
-            orders = rec.order_ids.filtered(lambda r: r.state in ["sale", "done"])
-            rec.last_order_id = orders and orders[-1] or False
+    customer_order_count = Integer(string="Cantidad de pedidos", related="commercial_partner_id.sale_order_count", store=True)
