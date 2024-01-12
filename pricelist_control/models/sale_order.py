af459994a42398ce5af0cc90386a8cbcfd51083a
Federico Gobea <127751411+making-fgobea@users.noreply.github.com>
2024-01-11 22:31:42 -0500

    ✨ fix pricelist control issues

--- pricelist_control/models/sale_order.py
+++ pricelist_control/models/sale_order.py
@@ -1,7 +1,6 @@
-from odoo import fields
 from odoo.models import Model
 from odoo.exceptions import UserError
-from odoo.fields import Boolean
+from odoo.fields import Boolean, float_compare
 from odoo.api import onchange, depends
 import logging
 _logger = logging.getLogger(__name__)
@@ -10,35 +9,17 @@ class SaleOrderLine(Model):
         
     not_aligned_with_pricelist = Boolean(string="No coincide con lista de precios", compute="_compute_not_aligned_with_pricelist")
     
-    @depends('price_unit','discount')
+    @depends('price_unit','discount', 'product_uom_qty')
     def _compute_not_aligned_with_pricelist(self):
         for rec in self:
             rec.not_aligned_with_pricelist = rec._product_pricelist_price_differs_from_price_unit()
 
     def _product_pricelist_price_differs_from_price_unit(self):
-        pricelist_price_unit = self.product_id._get_tax_included_unit_price(
-            self.company_id,
-            self.order_id.currency_id,
-            self.order_id.date_order,
-            'sale',
-            fiscal_position=self.order_id.fiscal_position_id,
-            product_price_unit=self._get_display_price(),
-            product_currency=self.order_id.currency_id
-        )
         if self.order_id.pricelist_id.discount_policy == 'without_discount':
-            product_context = dict(self.env.context, partner_id=self.order_id.partner_id.id, date=self.order_id.date_order, uom=self.product_uom.id)
-            price, rule_id = self.order_id.pricelist_id.with_context(product_context).get_product_price_rule(self.product_id, self.product_uom_qty or 1.0, self.order_id.partner_id)
-            new_list_price, currency = self.with_context(product_context)._get_real_price_currency(self.product_id, rule_id, self.product_uom_qty, self.product_uom, self.order_id.pricelist_id.id)
-            if self.order_id.pricelist_id.currency_id != currency:
-                # we need new_list_price in the same currency as price, which is in the SO's pricelist's currency
-                new_list_price = currency._convert(
-                    new_list_price, self.order_id.pricelist_id.currency_id,
-                    self.order_id.company_id or self.env.company, self.order_id.date_order or fields.Date.today())
-            discount = ((new_list_price - price) / (new_list_price )) * 100
-            if self.price_unit == pricelist_price_unit and discount == self.discount:
+            if float_compare(self.price_unit, self._get_pricelist_price_before_discount(), precision_digits=2) == 0 and float_compare(self.pricelist_item_id.price_discount, self.discount, precision_digits=2) == 0:
                 return False
         if self.order_id.pricelist_id.discount_policy == 'with_discount':
-            if self.price_unit == pricelist_price_unit and self.discount == 0:
+            if float_compare(self.price_unit, self._get_pricelist_price(), precision_digits=2) == 0 and float_compare(self.discount, 0, precision_digits=2) == 0:
                 return False
         return True
 
