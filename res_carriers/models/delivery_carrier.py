419e6f490378c8ab0bff5a3d4430c11ada42cce1
MRIVERA <mrivera@making.com.ar>
2024-05-07 14:19:08 -0300

    🔥

--- res_carriers/models/delivery_carrier.py
+++ res_carriers/models/delivery_carrier.py
@@ -12,13 +12,13 @@ class res_Carrier(models.Model):
     _order = 'sequence, id'
 
     name = fields.Char('Expreso', required=True, translate=True)
-    razon_social = fields.Char('Razón Social')
-    direccion = fields.Char('Dirección')
-    ciudad = fields.Char('Ciudad')
+    company_name = fields.Char('Razón Social')
+    address = fields.Char('Dirección')
+    city = fields.Char('Ciudad')
     zip = fields.Char('Código Postal')
     state_id = fields.Many2one('res.country.state', 'Provincia')
-    telefono = fields.Char('Teléfono')
-    movil = fields.Char('Móvil')
+    telephone = fields.Char('Teléfono')
+    cellphone = fields.Char('Móvil')
     email =fields.Char('Correo electrónico')
     active = fields.Boolean(default=True)
     sequence = fields.Integer(help="Determine the display order", default=10)
@@ -26,15 +26,3 @@ class res_Carrier(models.Model):
     country_ids = fields.Many2many('res.country', 'res_carrier_country_rel', 'carrier_id', 'country_id', 'Countries')
     state_ids = fields.Many2many('res.country.state', 'res_carrier_state_rel', 'carrier_id', 'state_id', 'States')
     contact_address_complete = fields.Char(compute='_compute_contact_address', string='Dirección completa')
-    
-    @api.depends('direccion', 'zip', 'ciudad' )
-    def _compute_contact_address(self):
-        for record in self:
-            record.contact_address_complete = ''
-            if record.direccion:
-                record.contact_address_complete += record.direccion + ', '
-            if record.ciudad:
-                record.contact_address_complete += record.ciudad + ', '
-            if record.state_id:
-                record.contact_address_complete += record.state_id.name + ', '
-            record.contact_address_complete = record.contact_address_complete.strip().strip(',')
