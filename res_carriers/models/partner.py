95d2172eea419c752fec59242aad04b66fea54e8
MRIVERA <mrivera@making.com.ar>
2024-05-09 09:40:23 -0300

    🐛 fixing naming

--- res_carriers/models/partner.py
+++ res_carriers/models/partner.py
@@ -8,12 +8,3 @@ class ResPartner(models.Model):
     _inherit = 'res.partner'
 
     carrier_id = fields.Many2one('delivery.carrier.express',  string="Expreso")
-
-
-    @api.depends(lambda self: self._display_address_depends())
-    def _compute_contact_address(self):
-        for partner in self:
-            if not partner.carrier_id or not partner.carrier_id.contact_address_complete:
-                super(ResPartner, partner)._compute_contact_address()
-            else:
-                partner.contact_address = partner.carrier_id.contact_address_complete
