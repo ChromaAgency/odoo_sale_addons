3b9c7f5bc94080c059621c07d50e2ba932dc574b
MRIVERA <mrivera@chroma.agency>
2024-10-10 09:18:23 -0300

    ✨

--- res_carriers/models/delivery_carrier.py
+++ res_carriers/models/delivery_carrier.py
@@ -2,6 +2,7 @@
 
 import logging
 from odoo import api, fields, models
+from odoo.api import depends
 
 _logger = logging.getLogger(__name__)
 
@@ -22,6 +23,13 @@ class res_Carrier(models.Model):
     email =fields.Char('Correo electrónico')
     active = fields.Boolean(default=True)
     sequence = fields.Integer(help="Determine the display order", default=10)
+    complete_address = fields.Char(string="Dirección Completa", compute="_compute_complete_address", store=True)
+    country_id = fields.Many2one('res.country', string="País")
 
-    country_ids = fields.Many2many('res.country', 'res_carrier_country_rel', 'carrier_id', 'country_id', 'Countries')
+    @depends('street', 'city', 'state_id', 'zip', 'country_id')
+    def _compute_complete_address(self):
+        for carrier in self:
+            carrier.complete_address = ', '.join([carrier.street, carrier.city, carrier.state_id.name, carrier.zip, carrier.country_id.name])
+
+    country_ids = fields.Many2many('res.country', 'res_carrier_country_rel', 'carrier_id', 'country_id', 'Countries') # is this a nonesense?
     state_ids = fields.Many2many('res.country.state', 'res_carrier_state_rel', 'carrier_id', 'state_id', 'States')
